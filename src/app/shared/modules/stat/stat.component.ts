8d7273eaa418c53b0ed97c11d27d0282e40be573
Sławomir Simla <slawiks@wp.pl>
2019-11-06 18:09:14 +0100

    uzupełnineinie

--- src/app/shared/modules/stat/stat.component.ts
+++ src/app/shared/modules/stat/stat.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input, Output, EventEmitter, Directive} from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter} from '@angular/core';
 
 
 @Component({
